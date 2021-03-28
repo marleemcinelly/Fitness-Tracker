f04cf8e83d2d8c7cc8105fa845f0d71ccae9811e
Marlee McInelly <marleemcinelly@gmail.com>
2021-03-28 13:59:32 -0700

    testing api route edits to see if heroku error is in html or api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,48 +1,48 @@
-const path = require("path");
+// const path = require("path");
 
-const Workout = require("../models/Workout.js");
+// const Workout = require("../models/Workout.js");
 
-module.exports = function (app) {
+// module.exports = function (app) {
 
-    app.get("/api/workouts", (req, res) => {
-        Workout.find()
-            .then(data => {
-                res.json(data);
-            })
-            .catch(err => {
-                res.json(err);
-            });
-    });
+//     app.get("/api/workouts", (req, res) => {
+//         Workout.find()
+//             .then(data => {
+//                 res.json(data);
+//             })
+//             .catch(err => {
+//                 res.json(err);
+//             });
+//     });
 
-    app.create("/api/workouts/", (req, res) => {
-        Workout.create({})
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-    });
+//     app.create("/api/workouts/", (req, res) => {
+//         Workout.create({})
+//         .then(data => {
+//             res.json(data);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+//     });
     
-    router.get('/api/workouts/range', (req, res) => {
-        Workout.aggregate([
-          {
-            $addFields: {
-              totalDuration: {
-                $sum: '$exercises.duration',
-              },
-            },
-          },
-        ])
-          .sort({ _id: -1 })
-          .limit(7)
-          .then((dbWorkouts) => {
-            console.log(dbWorkouts);
-            res.json(dbWorkouts);
-          })
-          .catch((err) => {
-            res.json(err);
-          });
-      });
+//     router.get('/api/workouts/range', (req, res) => {
+//         Workout.aggregate([
+//           {
+//             $addFields: {
+//               totalDuration: {
+//                 $sum: '$exercises.duration',
+//               },
+//             },
+//           },
+//         ])
+//           .sort({ _id: -1 })
+//           .limit(7)
+//           .then((dbWorkouts) => {
+//             console.log(dbWorkouts);
+//             res.json(dbWorkouts);
+//           })
+//           .catch((err) => {
+//             res.json(err);
+//           });
+//       });
 
-};
+// };
